b7f0de185f81ca8969b83ca23703bd1df83c7820
Rafal Walasek <magician1590@gmail.com>
2025-07-22 12:55:11 +0200

    koniec projektu system platnosci

--- src/paymentSystem/PayApp.java
+++ src/paymentSystem/PayApp.java
@@ -2,6 +2,9 @@ package paymentSystem;
 
 public class PayApp {
     public static void main(String[] args) {
-
+        TransactionProcessor tp = new TransactionProcessor();
+        tp.processPayment(new PayPalPayment(), 400);
+        tp.processPayment(new BankTransferPayment(), 2000);
+        tp.processPayment(new CreditCardPayment(), 40000);
     }
 }
--- src/paymentSystem/TransactionProcessor.java
+++ src/paymentSystem/TransactionProcessor.java
@@ -1,9 +1,7 @@
 package paymentSystem;
 
 public class TransactionProcessor {
-
-
     public void processPayment(PaymentMethod method, double amount) {
-
+        method.pay(amount);
     }
 }
